fa8697fb798d82635cd2e8ecd531793ae399787c
Jenaro Wachira <wjenaro@gmail.com>
2023-06-28 00:43:05 +0300

    first commit

---
+++ day1OfChallenge.py
@@ -0,0 +1,12 @@
+#Band Name Generator
+print("Welcome to the Band Name Generator")
+city=input("What the name of the city you grew up  ")
+print(city)
+pet=input("What was you pet's name ")
+print(pet)
+'''here is the name of your band'''
+print(f"Your band name could be {city} {pet}")
+
+'''
+Day one challange date 28/6/2023
+'''
