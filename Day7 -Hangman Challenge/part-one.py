e95e7e074a5d09e68e25a80f705ba2fbbd48c232
Jenaro Wachira <wjenaro@gmail.com>
2023-07-28 08:11:45 +0300

    Jul 28 2023 - Hangman challenge - simple modeles coding

---
+++ Day7 -Hangman Challenge/part-one.py
@@ -0,0 +1,25 @@
+import random
+word_list=['Mama', 'Papa', 'House']
+#print(len(word_list))
+
+#select random word.
+random_word_id= random.randint(0, len(word_list)-1)
+
+#print(random_word_id)
+
+#the selected string
+myword=word_list[random_word_id]
+print(myword)
+#the length of my word
+print(len(myword))
+#convert to string
+#print(myword)
+#check letter in the word
+letter=input("guess letter: ")
+#print(len(myword))
+
+if letter in myword.lower():
+    print (" you won")
+else:
+    print("lost")
+
