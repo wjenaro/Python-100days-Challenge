b90193610486ac3f146a48bbc2ea55d3c1886018
Jenaro Wachira <wjenaro@gmail.com>
2023-08-11 11:43:18 +0300

    ago 11

---
+++ day10 - building calculator App/format_name.py
@@ -0,0 +1,19 @@
+def format_name(f_name,s_name):
+    format_fname=f_name.title()
+    format_sname=s_name.title()
+
+    return f"{format_fname} {format_sname}"
+
+'''
+What is title() in Python?
+A title-cased string is the one in which every first letter of the word is an uppercase character. Python title() function is used to convert an input string to a title case i.e. converting the first alphabet of each word to uppercase and the rest into lowercase. Python title() function ignore any non-alphabetic characters. It ignores any numbers and special characters in the string.
+
+Q2. How do you add a title in Python?
+To add a title to an input string in Python, we implement the title() string class method. Python title() function follows the below-mentioned syntax:
+
+                    
+string.title()
+'''
+
+
+
