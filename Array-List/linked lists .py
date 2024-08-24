4e88d8e6a2f87ee719c80bca3e03812418d5d022
Jenaro Wachira <wjenaro@gmail.com>
2024-08-24 15:39:08 +0300

    Linked List in python  -leetcode challange

---
+++ Array-List/linked lists .py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=' -> ' if current.next else '')
+            current = current.next
+        print()
+# Create a new LinkedList
+linked_list = LinkedList()
+
+# Append elements to the list
+linked_list.append(10)
+linked_list.append(20)
+linked_list.append(30)
+
+# Display the linked list
+linked_list.display()  # Output: 10 -> 20 -> 30
